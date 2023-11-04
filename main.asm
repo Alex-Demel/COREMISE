335acfc4fd7048c9199658bb93a92057690c40fa
Alex Demel <111302837+Alex-Demel@users.noreply.github.com>
2023-11-03 22:29:51 -0400

    Made subroutines modular

--- main.asm
+++ main.asm
@@ -17,27 +17,26 @@
                                             ; referencias a la sección actual.
 
 ;-------------------------------------------------------------------------------
-
-pos1	.equ	9      ; Alfanumerico A1 comienza en S18
-pos2	.equ	5      ; Alfanumerico A2 comienza en S10
-pos3 	.equ	3      ; Alfanumerico A3 comienza en S6
-pos4 	.equ	18     ; Alfanumerico A4 comienza en S36
-pos5	.equ	14     ; Alfanumerico A5 comienza en S28
-pos6 	.equ	7      ; Alfanumerico A6 comienza en S14
+;Definir pocisiones del LCD
+;pos          0 2 4 8  10 12
+pos		.word 9,5,3,18,14,7
 
 ;Definir high y low bytes para generar caracteres
-;char          0    1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16   17   18   19   20   21   22   23   24   25   26
-;             ' '   A    B    C    D    E    F    G    H    I    J    K    L    M    N    O    P    Q    R    S    T    U    V    W    X    Y    Z
-charH	.byte 0x00,0xEF,0xF1,0x9C,0xF0,0x9F,0x8E,0xBD,0x6F,0x90,0x78,0x0E,0x1C,0x6C,0x6C,0xFC,0xCF,0xFC,0xCF,0xB7,0x80,0x7C,0x0C,0x7C,0x00,0x00,0x90
-charL	.byte 0x00,0x00,0x50,0x00,0x50,0x00,0x00,0x00,0x00,0x50,0x00,0x22,0x00,0xA0,0x82,0x00,0x00,0x02,0x02,0x00,0x50,0x00,0x28,0x10,0xAA,0xB0,0x28
+;char          0    1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16   17   18   19   20   21   22   23   24   25   26   27   28
+;             ' '   A    B    C    D    E    F    G    H    I    J    K    L    M    N    O    P    Q    R    S    T    U    V    W    X    Y    Z    0    3
+charH	.byte 0x00,0xEF,0xF1,0x9C,0xF0,0x9F,0x8E,0xBD,0x6F,0x90,0x78,0x0E,0x1C,0x6C,0x6C,0xFC,0xCF,0xFC,0xCF,0xB7,0x80,0x7C,0x0C,0x7C,0x00,0x00,0x90,0xFC,0xF3
+charL	.byte 0x00,0x00,0x50,0x00,0x50,0x00,0x00,0x00,0x00,0x50,0x00,0x22,0x00,0xA0,0x82,0x00,0x00,0x02,0x02,0x00,0x50,0x00,0x28,0x10,0xAA,0xB0,0x28,0x28,0x00
 
 ;Definir high y low bytes para generar números
 ;num           0    1    2    3    4    5    6    7    8    9
 numH	.byte 0xFC,0x00,0xDB,0xF3,0x67,0xB7,0xBF,0xE0,0xFF,0xE7
 numL	.byte 0x28,0x50,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
 
+;Definir los índices del string 'Team 03'
+team		.word	0,0,0,0,0,20,5,1,13,0,27,28,0,0,0,0,0,-1
+
 ;Definir los índices del string 'Alex Demel'
-alexNombre	.word	0,0,0,0,0,0,1,12,5,24,0,4,5,13,5,12,0,0,0,0,0,0
+alex		.word	0,0,0,0,0,1,12,5,24,0,4,5,13,5,12,0,0,0,0,0,-1
 
 RESET       mov.w   #__STACK_END,SP         ; Inicializar stackpointer
 StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Parar watchdog timer
@@ -57,6 +56,88 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Parar watchdog timer
 			; Victor Cruz
 			; % de contribución
 
+			;Inicializar el LCD
+			CALL	#SetupLCD
+
+			;Dibujar Team03 en el LCD
+			MOV		#team,R8
+			CALL	#ScrollStr
+
+	        JMP		$
+			nop
+
+;-------------------------------------------------------------------------------
+;DrawChar
+;Objetivo: Dibujar un caracter en la pantalla en la posición dada
+;Precondiciones: La posición del caracter debe estar dada
+;en R4 y el índice del caracter debe estar dado en R5.
+;Postcondiciones: La pantalla mostrará el caracter
+;dado por R5 en la posición dada por R4.
+;Autor: Alex Demel
+;Fecha: 11/2/2023
+;-------------------------------------------------------------------------------
+DrawChar:
+  			MOV.B   charH(R5),0x0a20(R4)		;Accesar los segmentos (high/low)
+  												;guardados en el índice de R5 y
+  			MOV.B   charL(R5),0x0a20+1(R4)		;dibujar en la posición dada por
+  												;R4+0x0a20 y R4+0x0a20+1
+  			RET
+
+;-------------------------------------------------------------------------------
+;ScrollStr
+;Objetivo: Dibujar un string 'scrolling' en la pantalla
+;Precondiciones: La dirección del arreglo conteniendo el string debe estar dada en R8
+;Postcondiciones: La pantalla mostrará el string dado por R8 'scrolling'
+;Autor: Alex Demel
+;Fecha: 11/2/2023
+;-------------------------------------------------------------------------------
+ScrollStr:
+			PUSH	R4							;Guardar el contenido de R4
+			PUSH	R5							;Guardar el contenido de R5
+			PUSH	R6							;Guardar el contenido de R6
+			PUSH	R7							;Guardar el contenido de R7
+			PUSH	R9							;Guardar el contenido de R9
+
+restart:	MOV.B   #0,R6						;Usar R6 como índice de comienzo
+			MOV.B   #0,R9						;Usar R9 como contador
+
+first:		CALL	#delay						;Esperar antes de reescribir la pantalla
+			MOV.B   R6,R7						;Guardar R6 (ídice de comienzo) en R7 (índice general)
+
+next:		MOV.B   pos(R9),R4					;Guardar la pocisión del caracter en R4
+			MOV.W	R8,R5						;Guardar la dirección del arreglo en R5
+			ADD.W	R7,R5						;Sumar el índice a R5
+			MOV.W	@R5,R5						;Colocar el caracter apuntado por R5 en R5
+			CALL	#DrawChar
+
+			INCD.B	R9							;Incrementar contador
+			INCD.B	R7							;Incrementar el índice general
+
+	        CMP		#-1,R5						;Verificar si llegamos al último caracter
+			JZ		restart						;Regresar al inicio
+			CMP		#14,R9						;Verificar si llegamos a la última posición
+			JNZ		next						;Si no hemos llegado, continúa
+	        MOV.B   #0,R9						;Si llegamos al final, regresa a cero
+	        INCD.W	R6							;Incrementar R6 (ídice de comienzo)
+			JMP		first
+
+			POP		R9							;Recuperar el contenido de R9
+	        POP		R7							;Recuperar el contenido de R7
+	        POP		R6							;Recuperar el contenido de R6
+	        POP		R5							;Recuperar el contenido de R5
+	        POP		R4							;Recuperar el contenido de R4
+
+			RET
+
+;-------------------------------------------------------------------------------
+;SetupLCD
+;Objetivo: Hacer la inicialización del display
+;Precondiciones: No hay precondiciones
+;Postcondiciones: El LCD estará inicializado
+;Autor: Alex Demel
+;Fecha: 11/2/2023
+;-------------------------------------------------------------------------------
+SetupLCD:
 
 			;Inicializar segmentos 0 - 21; 26 - 43 del LCD
 			MOV.W   #0xFFFF,&LCDCPCTL0
@@ -85,136 +166,16 @@ UnlockGPIO  bic.w   #LOCKLPM5,&PM5CTL0      ; Desactivar el modo de alta impedan
 			;Encender LCD
 			BIS.W   #1,&LCDCCTL0
 
-			;Dibujar Team03 en el LCD
-			CALL	#Team03
-
-	        JMP		$
-			nop
-
-;Team03
-;Objetivo: Escribir TEAM03 en la pantalla
-;Precondiciones: El arreglo charH, charL, debe estar definido y el LCD debe estar activado
-;Postcondiciones: La pantalla mostrará el texto 'TEAM03'
-;Autor: Alex Demel
-;Fecha: 10/28/2023
-Team03:
-
-			PUSH	R5							;Guardar el contenido de R5
-			PUSH	R6							;Guardar el contenido de R6
-
-			MOV.B   #pos1,R6					;Colocar la posición del primer caracter en R6
-			MOV.W	#20,R5						;Colocar el índice del caracter a dibujar en R5
-
-  			MOV.B   charH(R5),0x0a20(R6)		;Accesar los segmentos (high/low) guardados en el índice de R5 y
-  			MOV.B   charL(R5),0x0a20+1(R6)		;dibujar en la posición dada por R6+0x0a20/R6+0x0a20+1
-
-			MOV.B   #pos2,R6
-			MOV.W	#5,R5
-			MOV.B   charH(R5),0x0a20(R6)
-	        MOV.B   charL(R5),0x0a20+1(R6)
-
-			MOV.B   #pos3,R6
-			MOV.W	#1,R5
-			MOV.B   charH(R5),0x0a20(R6)
-	        MOV.B   charL(R5),0x0a20+1(R6)
-
-	        MOV.B   #pos4,R6
-			MOV.W	#13,R5
-			MOV.B   charH(R5),0x0a20(R6)
-	        MOV.B   charL(R5),0x0a20+1(R6)
-
-	        MOV.B   #pos5,R6
-			MOV.W	#0,R5
-			MOV.B   numH(R5),0x0a20(R6)
-	        MOV.B   numL(R5),0x0a20+1(R6)
-
-	        MOV.B   #pos6,R6
-			MOV.W	#3,R5
-			MOV.B   numH(R5),0x0a20(R6)
-	        MOV.B   numL(R5),0x0a20+1(R6)
-
-	        POP		R6							;Recuperar el contenido de R6
-	        POP		R5							;Recuperar el contenido de R5
-
-	        RET
-
-;AlexDemel
-;Objetivo: Escribir 'Alex Demel' en la pantalla con scrolling
-;Precondiciones: El arreglo alexNombre, charH, charL, debe estar definido y el LCD debe estar activado
-;Postcondiciones: La pantalla mostrará el texto 'Alex Demel'
-;Autor: Alex Demel
-;Fecha: 10/28/2023
-AlexDemel:
-
-			PUSH	R5							;Guardar el contenido de R5
-			PUSH	R6							;Guardar el contenido de R6
-			PUSH	R7							;Guardar el contenido de R7
-			PUSH	R8							;Guardar el contenido de R8
-
-			MOV.B   #0,R7						;Usar R7 como índice de comienzo
-
-primero:	CALL	#delay						;Esperar antes de reescribir la pantalla
-			MOV.B   R7,R8						;Guardar R7 (ídice de comienzo) en R8 (índice general)
-
-			MOV.B   #pos1,R6
-			MOV.W	alexNombre(R8),R5
-			MOV.B   charH(R5),0x0a20(R6)
-	        MOV.B   charL(R5),0x0a20+1(R6)
-
-	        INCD.B	R8							;Incrementar R8 (índice general)
-
-	        MOV.B   #pos2,R6
-			MOV.W	alexNombre(R8),R5
-			MOV.B   charH(R5),0x0a20(R6)
-	        MOV.B   charL(R5),0x0a20+1(R6)
-
-	        INCD.B	R8							;Incrementar R8 (índice general)
-
-	        MOV.B   #pos3,R6
-			MOV.W	alexNombre(R8),R5
-			MOV.B   charH(R5),0x0a20(R6)
-	        MOV.B   charL(R5),0x0a20+1(R6)
-
-	        INCD.B	R8							;Incrementar R8 (índice general)
-	        MOV.B   #pos4,R6
-			MOV.W	alexNombre(R8),R5
-			MOV.B   charH(R5),0x0a20(R6)
-	        MOV.B   charL(R5),0x0a20+1(R6)
-
-	        INCD.B	R8							;Incrementar R8 (índice general)
-
-	        MOV.B   #pos5,R6
-			MOV.W	alexNombre(R8),R5
-			MOV.B   charH(R5),0x0a20(R6)
-	        MOV.B   charL(R5),0x0a20+1(R6)
-
-	        INCD.B	R8							;Incrementar R8 (índice general)
-
-	        MOV.B   #pos6,R6
-			MOV.W	alexNombre(R8),R5
-			MOV.B   charH(R5),0x0a20(R6)
-	        MOV.B   charL(R5),0x0a20+1(R6)
-
-	        INCD	R7							;Incrementar R7 (índice de comienzo)
-	        CMP		#34,R7						;Verificar si el nuevo índice de comienzo es válido
-	        JNZ		primero						;Volver al inicio
-	        MOV.B   #0,R7						;Si el índice no es válido, regresar a cero
-	        JMP		primero						;Volver al inicio
-
-	        POP		R9							;Recuperar el contenido de R9
-	        POP		R8							;Recuperar el contenido de R8
-	        POP		R7							;Recuperar el contenido de R7
-	        POP		R6							;Recuperar el contenido de R6
-	        POP		R5							;Recuperar el contenido de R5
-
 			RET
 
+;-------------------------------------------------------------------------------
 ;delay
 ;Objetivo: Que el programa espere un poco antes de continuar
-;Precondiciones: Ninguna
+;Precondiciones: No hay precondiciones
 ;Postcondiciones: El programa esperará un poco y continuará
 ;Autor: Alex Demel
 ;Fecha: 10/28/2023
+;-------------------------------------------------------------------------------
 delay:
 			PUSH	R4							;Guardar el contenido de R4
 
@@ -227,19 +188,6 @@ cuenta:		INC		R4							;Incrementar R4
 
 			RET
 
-;megaDelay
-;Objetivo: Que el programa espere varios segundos antes de continuar
-;Precondiciones: Ninguna
-;Postcondiciones: El programa esperará varios segundos y continuará
-;Autor: Alex Demel
-;Fecha: 10/28/2023
-megaDelay:
-			CALL #delay
-			CALL #delay
-			CALL #delay
-			CALL #delay
-
-			RET
 ;-------------------------------------------------------------------------------
 ; Definición de stackpointer
 ;-------------------------------------------------------------------------------
